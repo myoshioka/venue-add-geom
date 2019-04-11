001715e7f1aaafd6700fef6db80a6e373a8059af
myoshioka <y.mako2@gmail.com>
2019-04-11 20:02:35 +0900

    住所文字列を追加
     - 住所文字列を追加するように修正
     - フィアル名を入力できるように修正

--- venue_add_geom.py
+++ venue_add_geom.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 import pandas as pd
+import click
 
 dotenv_path = './.env'
 load_dotenv(dotenv_path)
@@ -13,24 +14,45 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-def main():
+@click.command()
+@click.option('--file-name', '-f',  help='input csv file')
+def main(file_name):
 
     logger.debug('start!')
-    df = pd.read_csv('./poi_ranking.csv')
+    df = pd.read_csv(file_name)
     out_list = []
     for index, row in df.iterrows():
         venue_id = row['current_venue_id']
         print(venue_id)
         venues_detail = FoursquareApi.get_foursquare_venue_details(venue_id)
-        out_list.append([index, venues_detail['name'], venues_detail['location']['lat'],
-                         venues_detail['location']['lng']])
+
+        state = None
+        city = None
+        address = None
+        if 'state' in venues_detail['location']:
+            state = venues_detail['location']['state']
+
+        if 'city' in venues_detail['location']:
+            city = venues_detail['location']['city']
+
+        if 'address' in venues_detail['location']:
+            address = venues_detail['location']['address']
+
+        out_list.append([
+            index, venues_detail['name'],
+            venues_detail['location']['lat'],
+            venues_detail['location']['lng'],
+            state,
+            city,
+            address
+        ])
 
     out_venue_df = pd.DataFrame(
-        out_list, columns=['id', 'venue_name', 'lat', 'lng'])
+        out_list, columns=['id', 'venue_name', 'lat', 'lng', 'state', 'city', 'address'])
 
     out_df = pd.merge(df, out_venue_df, how='left',
                       left_index=True, right_index=True)
-    out_df.to_csv('./poi_ranking_location.csv')
+    out_df.to_csv('./' + file_name.replace('.csv', '') + '_add_location.csv')
 
     logger.debug('end!')
 
